38180b598148c70af2b6f4e7fa7d5614ec815631
Jake Reck <jakereck@gmail.com>
2018-08-25 14:31:15 -0700

    Made a footer and style changes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const   express     = require('express'),
         bodyParser  = require('body-parser'),
         nodemailer  = require('nodemailer'),
-        xoauth2         = require('xoauth2'),
-
+        
         port = process.env.PORT || 3050,
         app = express();
 
@@ -18,48 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-    const output = `
-        <p>You have a new contact request</p>
-        <h3>Contact Details</h3>
-        <ul>
-            <li>Name: ${req.body.name}</li>
-            <li>Email: ${req.body.email}</li>
-            <li>Phone: ${req.body.phone}</li>
-            <p>Name: ${req.body.message}</p>
-        </ul>
-    `;
-
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            xoauth2: xoauth2.createXOAuth2Generator({
-                user: 'jakereck@gmail.com',
-                clientId: '645131124041-m4fn94kcevmfn8796juqhg3e0lrm57i9.apps.googleusercontent.com',
-                clientSecret: 'qhoFXuXj_7cZTmxPfWcrKctB',
-                refreshToken: '1/Mip28I-TqRqmJQS4BKHef6rQcxXMdYBocnImc8nGl9g',
-            })
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: req.body.email, // sender address
-        to: 'jakereck@gmail.com', // list of receivers
-        subject: 'Contact Request', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        
-        res.render('index', {msg: 'Email has been sent'});
-    });
+    
 });
 
 app.listen(port, () => {
