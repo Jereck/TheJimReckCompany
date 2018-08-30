f0cf8f5b0a6479e225cfe7a33ce0c35e3e6a7099
Jake Reck <jakereck@gmail.com>
2018-08-30 07:49:32 -0700

    Made some changes to mobile design

--- public/js/master.js
+++ public/js/master.js
@@ -1,7 +1,5 @@
 AOS.init();
 
-
-
 // Select all links with hashes
 $('a[href*="#"]')
     // Remove links that don't actually link to anything
@@ -10,9 +8,9 @@ $('a[href*="#"]')
     .click(function(event) {
         // On-page links
         if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-        && 
-        location.hostname == this.hostname
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
         ) {
         // Figure out element to scroll to
         var target = $(this.hash);
@@ -22,19 +20,20 @@ $('a[href*="#"]')
             // Only prevent default if animation is actually gonna happen
             event.preventDefault();
             $('html, body').animate({
-            scrollTop: target.offset().top
+                scrollTop: target.offset().top
             }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-            } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-            };
+                // Callback after animation
+                // Must change focus!
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                } else {
+                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                };
             });
         }
     }
 });
+
